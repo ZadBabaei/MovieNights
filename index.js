61d227ef5e2d987433eb399f22f3842eca36b43d
Zad-mhb <zadbabaei@gmail.com>
2020-11-03 13:31:01 -0330

    fixed auto complete search

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ function onLoad(){
   function onVoteButtonCliked (){
     let user=prompt("please enter your name")
   }
+  function onClickedSerchedItem(){
+    let changeImg=document.getElementsByClassName("thumbnail-01").src="Imges/01.jpg";
+
+  }
   voteButton.addEventListener("click",onVoteButtonCliked)
   let movieList=[
-    "1","11","111"
+    "chernobyl",
+    "the boys",
+    "good omens",
+    "carnival row",
+    "the dark knight",
+    "lord of the ring-the return of the kig",
+    "forrest gump",
+    "the star wars sage continues",
+    "seven",
+    "life is beautiful",
+    "the silence of the lambs",
+    "saving private ryan",
+    "green mile",
+    "leon the profetional",
+    "interstellar",
+    "the pianist",
+    "gladiator",
+    "joker",
+
+
   ]
   $( "#searchTerm" ).autocomplete({
     source: movieList
   });
+  $( ".selector" ).on( "autocompletefocus", onClickedSerchedItem( ) );
 
 
 }
